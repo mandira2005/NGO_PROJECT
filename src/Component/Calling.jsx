80cd6b7a1c989b2fcee1d97a3c5193234bc2470f
mandira2005 <pobimandira@gmail.com>
2026-09-07 21:34:25 +0530

    Added slider

--- src/Component/Calling.jsx
+++ src/Component/Calling.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MenuBar from './Menubar'
+import Slide from "./Slider";
 
 
 const Call = () => {
     return (
         <>
             <MenuBar/>
+            <Slide/>
         </>
     )
 }
