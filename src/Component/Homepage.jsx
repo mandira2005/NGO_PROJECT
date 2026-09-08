6a41b432ed6a46deb22ae152c2eab4a86684983f
mandira2005 <pobimandira@gmail.com>
2026-09-08 12:53:17 +0530

    Changes

--- src/Component/Homepage.jsx
+++ src/Component/Homepage.jsx
@@ -8,23 +8,25 @@ const Home = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-3 col-lg-3 col-sm-12 First">
-                            <i class="fa-solid fa-leaf"></i>
+                            <i className="fa-solid fa-leaf icon1"></i>
                             <h1>Reduce Food Waste</h1>
                             <h2>We collect surplus food from<br />restaurants, stores and farms.</h2>
                         </div>
 
                         <div className="col-md-3 col-lg-3 col-sm-12 Second">
-
+                            <i class="fa-solid fa-users icon2"></i>
                             <h1>Support Communities</h1>
                             <h2>We distributes nutritious food<br />to those in need.</h2>
                         </div>
 
                         <div className="col-md-3 col-lg-3 col-sm-12 Third">
+                            <i class="fa-solid fa-arrows-spin icon3"></i>
                             <h1>Create a Sustainable Future</h1>
                             <h2>Less waste means a healtier<br />planet.</h2>
                         </div>
 
                         <div className="col-md-3 col-lg-3 col-sm-12 Forth">
+                            <i class="fa-regular fa-heart icon4"></i>
                             <h1>Build Awareness</h1>
                             <h2>We educate and inspire<br />for lasting change.</h2>
                         </div>
