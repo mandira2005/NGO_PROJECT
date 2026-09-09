0104d84540b3b15664a545a687cd247e3d2213b4
mandira2005 <pobimandira@gmail.com>
2026-09-09 22:36:47 +0530

    Changes

--- src/Component/Homepage.jsx
+++ src/Component/Homepage.jsx
@@ -8,32 +8,33 @@ const Home = () => {
             <div className="container-fluid descrip-tion">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-3 col-lg-3 col-sm-12 First">
+                        <div className="col-md-3 col-lg-3 col-sm-12 First1">
                             <i className="fa-solid fa-leaf icon1"></i>
                             <h1>Reduce Food Waste</h1>
                             <h2>We collect surplus food from<br />restaurants, stores and farms.</h2>
                         </div>
 
-                        <div className="col-md-3 col-lg-3 col-sm-12 Second">
-                            <i class="fa-solid fa-users icon2"></i>
+                        <div className="col-md-3 col-lg-3 col-sm-12 First1">
+                            <i class="fa-solid fa-users icon1"></i>
                             <h1>Support Communities</h1>
                             <h2>We distributes nutritious food<br />to those in need.</h2>
                         </div>
 
-                        <div className="col-md-3 col-lg-3 col-sm-12 Third">
-                            <i class="fa-solid fa-arrows-spin icon3"></i>
+                        <div className="col-md-3 col-lg-3 col-sm-12 First1">
+                            <i class="fa-solid fa-arrows-spin icon1"></i>
                             <h1>Create a Sustainable Future</h1>
                             <h2>Less waste means a healtier<br />planet.</h2>
                         </div>
 
-                        <div className="col-md-3 col-lg-3 col-sm-12 Forth">
-                            <i class="fa-regular fa-heart icon4"></i>
+                        <div className="col-md-3 col-lg-3 col-sm-12 First2">
+                            <i class="fa-regular fa-heart icon1"></i>
                             <h1>Build Awareness</h1>
                             <h2>We educate and inspire<br />for lasting change.</h2>
                         </div>
                     </div>
                 </div>
             </div>
+
 
 
             <div className="container">
@@ -50,65 +51,74 @@ const Home = () => {
                 </div>
             </div>
 
+
+
+
             <div className="container-fluid descrip">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-4 col-lg-4 col-sm-12 Ft">
+                        <div className="col-md-4 col-lg-4 col-sm-12 Second1">
                             <h3>our impact</h3>
                             <h1>Real Food. Real Change.</h1>
                             <h2>Every meal saved makes a difference - for people and the planet</h2>
                         </div>
-                        <div className="col-md-2 col-lg-2 col-sm-12 Sd">
-                            <i className="fa-solid fa-bowl-rice icn1"></i>
+                        <div className="col-md-2 col-lg-2 col-sm-12 Second2">
+                            <i className="fa-solid fa-bowl-rice icn2"></i>
                             <h1>12,500+</h1>
                             <h2>Meals Distributed</h2>
                         </div>
-                        <div className="col-md-2 col-lg-2 col-sm-12 Td">
+                        <div className="col-md-2 col-lg-2 col-sm-12 Second2">
                             <i className="fa-solid fa-users icn2"></i>
                             <h1>4+</h1>
                             <h2>Partner Organisations</h2>
                         </div>
-                        <div className="col-md-2 col-lg-2 col-sm-12 Frth">
-                            <i className="fa-brands fa-envira icn3"></i>
+                        <div className="col-md-2 col-lg-2 col-sm-12 Second2">
+                            <i className="fa-brands fa-envira icn2"></i>
                             <h1>6,800+ kg</h1>
                             <h2>Food Saved</h2>
                         </div>
-                        <div className="col-md-2 col-lg-2 col-sm-12 Fth">
-                            <i className="fa-brands fa-pagelines icn4"></i>
+                        <div className="col-md-2 col-lg-2 col-sm-12 Second3">
+                            <i className="fa-brands fa-pagelines icn2"></i>
                             <h1>3+</h1>
                             <h2>Communities Supported</h2>
                         </div>
                     </div>
                 </div>
             </div>
+
+
+
+
             <h5>How it Works</h5>
             <h4>From Surplus to Support</h4>
             <div className="container-fluid">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-3 col-lg-3 col-sm-12 Snd">
-                            <i class="fa-solid fa-shop i1"></i>
+                        <div className="col-md-3 col-lg-3 col-sm-12 Third">
+                            <i class="fa-solid fa-shop i2"></i>
                             <h1>1. Collect</h1>
-                            <h2>We collect surplus food from local business,<br/> stores and farms.</h2>
+                            <h2>We collect surplus food from local business, stores and farms.</h2>
                         </div>
-                        <div className="col-md-3 col-lg-3 col-sm-12 Snd">
+                        <div className="col-md-3 col-lg-3 col-sm-12 Third">
                             <i class="fa-regular fa-truck i2"></i>
                             <h1>2. Sort & Ensure Safety</h1>
                             <h2>Food is checked, sorted, and made safe for consumption</h2>
                         </div>
-                        <div className="col-md-3 col-lg-3 col-sm-12 Trd">
-                            <i class="fa-solid fa-users-between-lines i3"></i>
+                        <div className="col-md-3 col-lg-3 col-sm-12 Third">
+                            <i class="fa-solid fa-users-between-lines i2"></i>
                             <h1>3. Distribute</h1>
                             <h2>We deliver the food to NGOs, shelters and communities in need.</h2>
                         </div>
-                        <div className="col-md-3 col-lg-3 col-sm-12 Frt">
-                            <i className="fa-brands fa-envira i4"></i>
+                        <div className="col-md-3 col-lg-3 col-sm-12 Third">
+                            <i className="fa-brands fa-envira i2"></i>
                             <h1>4. Create Impact</h1>
                             <h2>Less food waste, healthier communities and a greener planet.</h2>
                         </div>
                     </div>
                 </div>
             </div>
+
+
 
             <div className="container back">
                 <div className="col-md-12 col-lg-12 col-sm-12 txt1">
